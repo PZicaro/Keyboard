4906bb8f61dfe6d608091df589b973534f0a59d3
icaro <icaro2rodrigues@gmail.com>
2022-03-06 02:46:49 -0300

    Keyboard

---
+++ teclado/main.js
@@ -0,0 +1,48 @@
+const keys = document.querySelectorAll('.key')
+
+function playNote(event){
+
+        let audioKeyCode = getKeyCode(event);
+
+        
+        console.log(audioKeyCode)
+
+
+         const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+
+         const cantFoundAnyKey = !key
+
+         if(cantFoundAnyKey){
+            return
+        }
+        
+   playAudio(audioKeyCode)
+        }
+
+
+function getKeyCode(event){
+    let keyCode;
+
+    const isKeyboard = event.type === 'keydown'
+    if(isKeyboard){
+ 
+         keyCode = event.keyCode
+ 
+    } else{
+ 
+        keyCode = event.target.dataset.key
+ 
+    }
+    return keyCode
+}
+function playAudio(audioKeyCode){
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0;
+    audio.play()
+}
+
+keys.forEach(function(key){
+    key.addEventListener('click', playNote)
+}
+)
+window.addEventListener('keydown', playNote)
